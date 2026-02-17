40fd25644b20dbf975e44c4a705144c50448744b
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-02-17 18:39:17 +0100

    feat : class BDD pour se connecter à la database

---
+++ config/bdd.php
@@ -0,0 +1,18 @@
+<?php
+
+class BDD {
+
+    protected PDO $pdo;
+    private string $host = "localhost";
+    private string $username = "root";
+    private string $password = "";
+    private string $database = "quiznight";
+
+    public function __construct() {
+        try {
+            $this->pdo = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database . ";charset=utf8",$this->username,$this->password,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+}
