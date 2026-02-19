111ce2f8623f69986c5f4a39b720cf55f215e234
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-02-19 13:52:59 +0100

    feat : footer + page accueil avec css

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+</main>
+
+<footer>
+    <ul class="footer-icons">
+        <li>
+            <a href="">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fa-brands fa-youtube"></i>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fa-brands fa-x-twitter"></i>
+            </a>
+        </li>
+    </ul>
+    <p>© <?php echo date("Y") ?> OtakuQuiz - Tous droits réservés</p>
+</footer>
---
+++ pages/accueil.php
@@ -0,0 +1,28 @@
+<?php
+include '../includes/header.php';
+
+
+?>
+
+
+<section class="hero">
+    <ul class="hero-icons">
+        <li>
+            <i class="fa-solid fa-bolt"></i>
+        </li>
+        <li>
+            <i class="fa-solid fa-trophy"></i>
+        </li>
+        <li>
+            <i class="fa-solid fa-star"></i>
+        </li>
+    </ul>
+    <h2>Teste ta puissance d’Otaku Ultime</h2>
+    <p>Mesure tes connaissances sur les mangas et animes japonais avec nos quiz immersifs !</p>
+</section>
+
+<section class="quiz-section">
+    <h3>Quiz disponibles</h3>
+</section>
+
+<?php include '../includes/footer.php'; ?>
